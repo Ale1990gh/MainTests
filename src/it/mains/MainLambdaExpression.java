cf22d99a75074ee5d67561792aecabc1a2434b1e
Ale1990gh <alemarte1990@hotmail.com>
2023-09-13 16:59:15 +0200

    prova

---
+++ src/it/mains/MainLambdaExpression.java
@@ -0,0 +1,16 @@
+package it.mains;
+
+import java.util.ArrayList;
+
+public class MainLambdaExpression {
+
+	public static void main(String[] args) {
+		ArrayList<Character> characters = new ArrayList<Character>();
+	    characters.add('t');
+	    characters.add('e');
+	    characters.add('s');
+	    characters.add('t');
+	    characters.forEach( (c) -> { System.out.println(c); } );
+	}
+
+}
