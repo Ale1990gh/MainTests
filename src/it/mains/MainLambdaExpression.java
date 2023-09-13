651c4ca5d73b97ced9007e2e23fef3dff8ba4480
Ale1990gh <alemarte1990@hotmail.com>
2023-09-13 17:59:25 +0200

    prova

--- src/it/mains/MainLambdaExpression.java
+++ src/it/mains/MainLambdaExpression.java
@@ -7,7 +7,7 @@ public class MainLambdaExpression {
 	public static void main(String[] args) {
 		ArrayList<Character> characters = new ArrayList<Character>();
 	    characters.add('t');
-	    characters.add('e');
+	    characters.add('e');//
 	    characters.add('s');
 	    characters.add('t');
 	    characters.forEach( (c) -> { System.out.println(c); } );
