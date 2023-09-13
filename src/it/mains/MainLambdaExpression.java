dd315a014d8a4914f0b762141acc181f62c508e9
Ale1990gh <alemarte1990@hotmail.com>
2023-09-13 17:44:25 +0200

    commit

--- src/it/mains/MainLambdaExpression.java
+++ src/it/mains/MainLambdaExpression.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class MainLambdaExpression {
 
 	public static void main(String[] args) {
-//		ArrayList<Character> characters = new ArrayList<Character>();
-//	    characters.add('t');
-//	    characters.add('e');
-//	    characters.add('s');
-//	    characters.add('t');
-//	    characters.forEach( (c) -> { System.out.println(c); } );
+		ArrayList<Character> characters = new ArrayList<Character>();
+	    characters.add('t');
+	    characters.add('e');
+	    characters.add('s');
+	    characters.add('t');
+	    characters.forEach( (c) -> { System.out.println(c); } );
 	}
 
 }
